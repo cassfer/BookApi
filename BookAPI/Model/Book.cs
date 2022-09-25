0adece5e9220e13d408e62876963e75a7a460359
cassfern <cassiohafernandes@gmail.com>
2022-09-25 17:59:07 -0300

    Models Book, BookContext and add the packages to connect to SQL

---
+++ BookAPI/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookAPI.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public int Description { get; set; }
+        public int Title { get; set; }
+    }
+}
